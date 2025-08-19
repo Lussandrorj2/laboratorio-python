6df5e496d9bebaf60f2691540d78efaa356fb914
Lussandro <lussandrorj2@hotmail.com>
2025-08-19 20:46:06 -0300

    add informações sobre try e except value error

--- exercicios.py/exercicio01.py
+++ exercicios.py/exercicio01.py
@@ -1,11 +1,11 @@
-print("### Tabuada ###")
+print("### Tabuada ###") 
 while True:
-    try:
+    try: #Trato o erro ValueError caso o usuário digite algo que não seja um número
         num = int(input("Digite um número (ou digite 0 para sair): "))
         if num == 0:
             print("saindo do programa...")
             break
-    except ValueError:
+    except ValueError: #Junto ao try trato o erro em caso o usuário digite algo que não seja o número
         print("Por favor, digite um número válido.")
         continue
     for i in range(1,11):
