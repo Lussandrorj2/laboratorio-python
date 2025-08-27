5a8e39ba40ffe2f720a6562851327eff8e4b43ac
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2025-08-27 08:43:43 +0000

    criando programa class_triangulo

---
+++ class_exercicio/class_triangulo.py
@@ -0,0 +1,14 @@
+class triangulo:
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
+
+    def area(self):
+        return self.largura * self.altura
+    
+    def perimetro(self):
+        return 2*(self.largura + self.altura)
+    
+r = triangulo(4,5)
+print(f"Area:",r.area())
+print(f"Perímetro:",r.perimetro())
